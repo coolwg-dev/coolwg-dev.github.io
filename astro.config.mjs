ffd583fce4a01abafcc74df01a5ae05fd98d7e6f
coolwg-dev <yumancolman@gmail.com>
2026-02-20 17:30:27 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,5 @@ import { defineConfig } from 'astro/config';
 // Set the production site URL for Astro (used for absolute URLs and sitemap)
 export default defineConfig({
   site: 'https://coolwg-dev.github.io',
+  outDir: 'docs',
 });
